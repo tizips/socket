bbcab7757cc97b57ffcbab7885b6ff8cca7dc7c0
tizips <tizips@tizipsdeiMac.local>
2018-01-16 16:51:18 +0800

    添加命令参数监控端口

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	_ "socket/routers"
-
 	"github.com/astaxie/beego"
+	"flag"
 )
 
 func main() {
 
+	port := flag.Int("port", 9105, "http port")
+	flag.Parse()
+
+	beego.BConfig.Listen.HTTPPort = *port
 	beego.Run()
 }
